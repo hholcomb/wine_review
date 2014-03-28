cb2f5041fea70a43ae2a44436ed93d1691eadd09
Howard <sixkids7@earthlink.net>
2014-03-28 07:20:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WineReview::Application.config.secret_key_base = 'c23c6ccb5bd8b69061643e48b6c0e3876c2fc2121a74b375a6d5333d3bb55b84a88aa822c34574c8f4e428bdc0a75586c9cdc890ba8ac16ad721aaea7fc20f70'
