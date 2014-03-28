49c695055f6b630ed013dc14413284a726f6a63a
Howard <sixkids7@earthlink.net>
2014-03-28 19:46:47 -0400

    initial DB version

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Wine.create(name: 'Wine 1b', year: 1940, winery: 'Winery 1b Master', country: 'USA', varietal: 'Cabernet')
+Wine.create(name: 'Wine 2c', year: 1950, winery: 'Winery 2c Master', country: 'France', varietal: 'Chardonnay')
+Wine.create(name: 'Wine 3d', year: 1960, winery: 'Winery 3d Master', country: 'Germany', varietal: 'Merlot')
+Wine.create(name: 'Wine 4e', year: 1970, winery: 'Winery 4e Master', country: 'Australia', varietal: 'Zinfandel')
+Wine.create(name: 'Wine 5f', year: 1980, winery: 'Winery 5f Master', country: 'Spain', varietal: 'Tempranilla')
+
